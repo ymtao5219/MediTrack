a96c2c3b93849594ae076281568959730f14b4b9
洪立軒 <u8351574@gmail.com>
2023-11-25 15:13:06 -0800

    Initial with Java Spring Maven

---
+++ Medi_backend/src/main/java/MediTrack/Medi/MediApplication.java
@@ -0,0 +1,13 @@
+package MediTrack.Medi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediApplication.class, args);
+	}
+
+}
