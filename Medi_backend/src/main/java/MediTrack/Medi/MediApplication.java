b1c44ad73f798ca45051090327dd67d91975ee77
洪立軒 <u8351574@gmail.com>
2023-11-25 15:52:25 -0800

    Connect with Mongodb

--- Medi_backend/src/main/java/MediTrack/Medi/MediApplication.java
+++ Medi_backend/src/main/java/MediTrack/Medi/MediApplication.java
@@ -2,7 +2,11 @@ package MediTrack.Medi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 @SpringBootApplication
 public class MediApplication {
 
@@ -10,4 +14,8 @@ public class MediApplication {
 		SpringApplication.run(MediApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String apiRoot(){
+		return "Hello World";
+	}
 }
