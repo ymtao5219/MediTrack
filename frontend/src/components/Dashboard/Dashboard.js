4c6a68249827fe5f13c6ec65badb90c7b97d284f
Yiming Yuan <ym.yuan1999@gmail.com>
2023-12-08 20:07:00 -0500

    add style to dash cards

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -1,4 +1,3 @@
-// src/components/Dashboard/Dashboard.js
 import React from 'react';
 import './Dashboard.css';
 
@@ -6,16 +5,20 @@ function Dashboard() {
   return (
     <div className="dashboard-container">
       <div className="dashboard-content">
-        <section className="patient-list">
-          <header>
+        <div className="patient-list dashboard-card">
+          <div className="patient-list-header">
             <h3>Patient List</h3>
-            <button>Add new Patient</button>
-            <button>Add new Event</button>
-          </header>
-          <p>No Patients</p>
-          {/* Patient list data will go here */}
-        </section>
-        <aside className="schedule">
+          </div>
+          <div className="patient-list-content">
+            {/* Patient list data will go here */}
+            <p>No Patients</p>
+          </div>
+          <div className="patient-list-footer">
+            <button className="button">Add new Patient</button>
+            <button className="button">Add new Event</button>
+          </div>
+        </div>
+        <aside className="schedule dashboard-card">
           <header>
             <h3>Schedule</h3>
             <a href="/see-all">See all</a>
