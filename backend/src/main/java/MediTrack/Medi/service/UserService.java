531639ae216bd31fa24e7804a0d44697858b4b3e
ByteYJ <yajieli@umass.edu>
2023-12-09 22:08:53 -0500

    Added UserService to encapsulate the business logic for user management.

---
+++ backend/src/main/java/MediTrack/Medi/service/UserService.java
@@ -0,0 +1,51 @@
+package MediTrack.Medi.service;
+
+import MediTrack.Medi.model.User;
+import MediTrack.Medi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class  UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findUserById(String id) {
+        return userRepository.findById(id);
+    }
+
+    public User updateUser(String id, User userDetails) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        user.setUsername(userDetails.getUsername());
+        user.setHashedPassword(userDetails.getHashedPassword());
+        user.setUserType(userDetails.getUserType());
+        user.setReferenceId(userDetails.getReferenceId());
+
+        return userRepository.save(user);
+    }
+
+    public boolean deleteUser(String id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            userRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+
+}
