9cb4ec903e76372601e35b4648c5d61a0273e4ac
Yiming Yuan <ym.yuan1999@gmail.com>
2023-12-10 17:12:33 -0500

    doctor retrieve patients

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
 import './Dashboard.css';
+import axios from 'axios';
 
+// Dashboard component
 function Dashboard() {
+  const [patients, setPatients] = useState([]);
+  // doctorId = '61a8b9e7f1b74e5f4c8b4579'; // Replace with actual doctor ID
+  useEffect(() => {
+    const fetchPatients = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8080/doctors/61d5d3e12345678912345678`);
+        console.log(response.data);
+        setPatients(response.data); // Assuming the API returns an array of patients
+      } catch (error) {
+        console.error('Error fetching patient data:', error);
+      }
+    };
+
+    fetchPatients();
+  }, []); // The effect runs when the doctorId changes
+
   return (
     <div className="dashboard-container">
       <div className="dashboard-content">
@@ -10,8 +28,17 @@ function Dashboard() {
             <h3>Patient List</h3>
           </div>
           <div className="patient-list-content">
-            {/* Patient list data will go here */}
-            <p>No Patients</p>
+          {patients.length > 0 ? (
+              <ul>
+                {patients.map((patient) => (
+                  <li key={patient.id}>
+                    {patient.name} - {patient.appointmentDate} {/* Adjust based on actual patient object */}
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>No patients scheduled 1234.</p>
+            )}
           </div>
           <div className="patient-list-footer">
             <button className="button">Add new Patient</button>
