f9425efd77b61057bbab3d0265b5b5ca60bfa6fa
洪立軒 <u8351574@gmail.com>
2023-12-10 22:41:03 -0500

    Fix CORS issue

--- backend/src/main/java/MediTrack/Medi/controller/AppointmentController.java
+++ backend/src/main/java/MediTrack/Medi/controller/AppointmentController.java
@@ -7,6 +7,7 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,12 +17,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import MediTrack.Medi.model.Appointment;
 import MediTrack.Medi.service.AppointmentService;
 
 @RestController
 @RequestMapping("/appointments")
+@CrossOrigin(origins = "*")
 public class AppointmentController {
     @Autowired
     private AppointmentService appointmentService;
--- backend/src/main/java/MediTrack/Medi/controller/MedicalRecordController.java
+++ backend/src/main/java/MediTrack/Medi/controller/MedicalRecordController.java
@@ -7,6 +7,7 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +22,7 @@ import MediTrack.Medi.service.MedicalRecordService;
 import jakarta.validation.Valid;
 
 
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/patients/{patientid}/medicalrecords")
 public class MedicalRecordController {
