9d4ca4d12a7be42696c757b766291025d3224831
Yiming Yuan <ym.yuan1999@gmail.com>
2023-12-14 17:00:43 -0500

    combine the login and dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 // App.js
 
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar'; 
@@ -8,18 +8,38 @@ import Login from './components/Login/Login';
 import Signup from './components/Signup/Signup'; 
 import Dashboard from './components/Dashboard/Dashboard'; 
 import WelcomeBanner from './components/WelcomeBanner/WelcomeBanner';
+import axios from 'axios'; 
 
 function App() {
   const [user, setUser] = useState(null);
   const [view, setView] = useState('login'); // State to manage login or signup view
   const [currentView, setCurrentView] = useState('patients');
-  
-  const handleLogin = (username, password, isDoctor) => {
-    // Your login logic here
-    setUser({
-      username: username,
-      userType: isDoctor ? 'Doctor' : 'Patient'
-    });
+   
+  useEffect(() => {
+    // If user is set (i.e., we have the userId), fetch user details
+    if (user && user.userId) {
+      axios.get(`http://localhost:8080/users/${user.userId}`)
+        .then(response => {
+          // Update the user state with the fetched details
+          setUser(prevUser => ({ ...prevUser, ...response.data }));
+        })
+        .catch(error => {
+          console.error('Error fetching user details:', error);
+        });
+    }
+  }, [user?.userId]);
+
+  const handleLogin = (loginResponse) => {
+    const userId = loginResponse.userId;
+    const userType = loginResponse.userType;
+    const referenceId = loginResponse.referenceId;
+
+    if (userId) {
+      setUser({ userId, userType });
+    } else {
+      // Handle error - no userId returned
+      console.error('No userId returned from login API');
+    }
   };
 
   const handleSignup = (username, password, email) => {
@@ -43,6 +63,8 @@ function App() {
   }
 
   // User is logged in, show the main content
+  console.log('check user');
+  console.log(user); 
   return (
     <div className="App">
       <Header userName={user ? user.username : 'anonymity'} userType={user.userType} />
@@ -50,7 +72,7 @@ function App() {
       <div className="main-content">
       {user && <Sidebar userType={user.userType} onSidebarClick={handleSidebarClick} />}
         <div className="content">
-          <Dashboard currentView={currentView} />
+          <Dashboard currentView={currentView} userId={user.referenceId} />
         </div>
       </div>
     </div>
