841d4e36a94fc4f94cecb8c81248d206c374896c
Yuanming Tao <ytao5219@gmail.com>
2023-12-14 05:11:21 -0500

    add icons for side bar items and responsive design

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -6,9 +6,7 @@ function Sidebar({ userType, onSidebarClick }) {
   const [activeItem, setActiveItem] = useState(null);
 
   const handleSidebarClick = (item) => {
-    // Call the passed in function from parent component if necessary
     onSidebarClick(item);
-    // Set the clicked item as active
     setActiveItem(item);
   };
 
@@ -17,41 +15,46 @@ function Sidebar({ userType, onSidebarClick }) {
       <div className="logo-container">
         <img src={logo} alt="Patient Tracker Logo" className="sidebar-logo" />
       </div>
-      <div className="sidebar-title-container">
-        <nav className="nav">
-          <ul className="nav-list">
-            {userType === 'patient' && (
-              <>
-                <li className={`nav-item ${activeItem === 'patientsInfo' ? 'active' : ''}`} onClick={() => handleSidebarClick('patientsInfo')}>
-                  <a href="#">Patient Info</a>
-                </li>
-                <li className={`nav-item ${activeItem === 'medicalRecords' ? 'active' : ''}`} onClick={() => handleSidebarClick('medicalRecords')}>
-                  <a href="#">Medical Records</a>
-                </li>
-                <li className={`nav-item ${activeItem === 'makeAppointment' ? 'active' : ''}`} onClick={() => handleSidebarClick('makeAppointment')}>
-                  <a href="#">Make Appointment</a>
-                </li>
-                <li className={`nav-item ${activeItem === 'patientAppointments' ? 'active' : ''}`} onClick={() => handleSidebarClick('patientAppointments')}>
-                  <a href="#">My Appointments</a>
-                </li>
-              </>
-            )}
-            {userType === 'doctor' && (
-              <>
-                <li className={`nav-item ${activeItem === 'doctorsInfo' ? 'active' : ''}`} onClick={() => handleSidebarClick('doctorsInfo')}>
-                  <a href="#">Doctor Info</a>
-                </li>
-                <li className={`nav-item ${activeItem === 'createMedicalRecord' ? 'active' : ''}`} onClick={() => handleSidebarClick('createMedicalRecord')}>
-                  <a href="#">Create Medical Record</a>
-                </li>
-                <li className={`nav-item ${activeItem === 'doctorAppointments' ? 'active' : ''}`} onClick={() => handleSidebarClick('doctorAppointments')}>
-                  <a href="#">Doctor Appointments</a>
-                </li>
-              </>
-            )}
-          </ul>
-        </nav>
-      </div>
+      <nav className="nav">
+        <ul className="nav-list">
+          {userType === 'patient' && (
+            <>
+              <li className={`nav-item ${activeItem === 'patientsInfo' ? 'active' : ''}`} onClick={() => handleSidebarClick('patientsInfo')}>
+                <i className="fa-solid fa-user"></i>
+                <a href="#">Patient Info</a>
+              </li>
+              <li className={`nav-item ${activeItem === 'medicalRecords' ? 'active' : ''}`} onClick={() => handleSidebarClick('medicalRecords')}>
+                <i className="fa-solid fa-file-medical"></i>
+                <a href="#">Medical Records</a>
+              </li>
+              <li className={`nav-item ${activeItem === 'makeAppointment' ? 'active' : ''}`} onClick={() => handleSidebarClick('makeAppointment')}>
+                <i className="fa-solid fa-calendar-plus"></i>
+                <a href="#">Make Appointment</a>
+              </li>
+              <li className={`nav-item ${activeItem === 'patientAppointments' ? 'active' : ''}`} onClick={() => handleSidebarClick('patientAppointments')}>
+                <i className="fa-solid fa-calendar-check"></i>
+                <a href="#">My Appointments</a>
+              </li>
+            </>
+          )}
+          {userType === 'doctor' && (
+            <>
+              <li className={`nav-item ${activeItem === 'doctorsInfo' ? 'active' : ''}`} onClick={() => handleSidebarClick('doctorsInfo')}>
+                <i className="fa-solid fa-user-md"></i>
+                <a href="#">Doctor Info</a>
+              </li>
+              <li className={`nav-item ${activeItem === 'createMedicalRecord' ? 'active' : ''}`} onClick={() => handleSidebarClick('createMedicalRecord')}>
+                <i className="fa-solid fa-notes-medical"></i>
+                <a href="#">Create Medical Record</a>
+              </li>
+              <li className={`nav-item ${activeItem === 'doctorAppointments' ? 'active' : ''}`} onClick={() => handleSidebarClick('doctorAppointments')}>
+                <i className="fa-solid fa-stethoscope"></i>
+                <a href="#">Doctor Appointments</a>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
       <div className="sidebar-bottom">
         {/* Add the logout or any other bottom content here */}
       </div>
