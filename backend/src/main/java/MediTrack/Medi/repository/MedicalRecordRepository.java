10185c1fc7145c027d1ded72fa61a63689efeb39
洪立軒 <u8351574@gmail.com>
2023-12-14 20:04:12 -0500

    Fix Medical Records

--- backend/src/main/java/MediTrack/Medi/repository/MedicalRecordRepository.java
+++ backend/src/main/java/MediTrack/Medi/repository/MedicalRecordRepository.java
@@ -1,8 +1,11 @@
 package MediTrack.Medi.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import MediTrack.Medi.model.MedicalRecord;
 
 public interface MedicalRecordRepository extends MongoRepository<MedicalRecord, String> {
+    List<MedicalRecord> findBydoctorId(String DoctorId);
 }
